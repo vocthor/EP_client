5f39437f678d183e11bf811c2db23660b9bf2796
vocthor <pieceo108@gmail.com>
2022-01-14 15:09:33 +0100

    init

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,19 +1,13 @@
 import React, { useState } from "react";
 
-export default function Calendar () {
+export default function Calendar() {
   const [cal, setCal] = useState([]);
   return (
     <div className="calendar">
       <h1>Emploi du temps</h1>
-      <img
-        src="html/images_ade/vocthor5698.gif"
-        height="450"
-        width="450"
-      />
+      <img src="../edt.jpg" height="450" />
     </div>
   );
-};
+}
 
-
-//ancien (deprecated) : src="http://applisjava.insa-rennes.fr/MonAde6Image/html/images_ade/HugoLAMOUREUX5413.gif" 
-
+//ancien (deprecated) : src="http://applisjava.insa-rennes.fr/MonAde6Image/html/images_ade/HugoLAMOUREUX5413.gif"
