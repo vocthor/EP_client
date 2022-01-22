2fa775ec874a017b320b7533d62e40876c8860bf
vocthor <pieceo108@gmail.com>
2022-01-23 00:58:00 +0100

    iframe (faut verif ac BDD + forme)

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
+import IframeResizer from "iframe-resizer-react";
 
-export default function Calendar() {
-  const [cal, setCal] = useState([]);
+export default function Calendar({ pseudo }) {
+
+  const URL = `http://applisjava.insa-rennes.fr/MonAde6Image/main?action=PSEUDO_RECHERCHE_AUTRE&param=0&pseudo=${pseudo}`;
+
+  //ATTENTION les dimensions de l'itemframe
+  //sont MALS FAITES (faudrait utiliser IframeResizer)
+  //TODO
   return (
     <div className="calendar">
       <h1>Emploi du temps</h1>
-      <img src="../edt.jpg" height="450" />
+      <IframeResizer log src={URL} height="870px" width="1550px" />
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,37 @@
 import Calendar from "../components/Calendar";
 import Navigation from "../components/Navigation";
 import Weather from "../components/Weather";
-import { useHistory } from "react-router-dom";
+import { useState , useEffect } from "react";
+import Axios from "axios";
 
 const Home = ({}) => {
-  let history = useHistory();
+  const [authState, setAuthState] = useState(false);
+  const [pseudo, setPseudo] = useState("");
+
+  /*  Permets de savoir s'il y a un user connecté au chargement de la page et donc de 
+  définir les constantes qui seront necessaires dans l'affichage */
+  useEffect(() => {
+    Axios.post(
+      "http://localhost:3001/getUserData",
+      { variable: 1 }, // je suis obligé de mettre ça sinon il y a un probleme avec le header mais en soit cette ligne est inutile
+      {
+        headers: {
+          accessToken: sessionStorage.getItem("accessToken"),
+        },
+      }
+    ).then((res) => {
+      if (res.data.error) {
+        setAuthState(false);
+      } else {
+        setAuthState(true);
+        setPseudo(res.data.pseudo)
+      }
+    });
+  },[]);
   return (
     <div className="home">
       <Navigation />
-      <Calendar />
+      {authState ? <Calendar pseudo={pseudo} /> : ""}
       <Weather />
     </div>
   );
