760d2ebe9e62be3d610fcf087cfc1b453e595f0a
vocthor <pieceo108@gmail.com>
2022-01-23 16:08:48 +0100

    iframe +- adaptatif

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,19 +1,24 @@
 import React from "react";
-import IframeResizer from "iframe-resizer-react";
 
 export default function Calendar({ pseudo }) {
-
+  
+  //URL de monADE selon le pseudo
   const URL = `http://applisjava.insa-rennes.fr/MonAde6Image/main?action=PSEUDO_RECHERCHE_AUTRE&param=0&pseudo=${pseudo}`;
 
-  //ATTENTION les dimensions de l'itemframe
-  //sont MALS FAITES (faudrait utiliser IframeResizer)
-  //TODO
+
+
+  /* ATTENTION
+Il n'est pas possible de redimensionner automatiquement le iframe selon son contenu
+ (il faudrait mettre un script dans la page ADE)
+ MAIS on peut gérer le zoom et les dimensions A LA MAIN depuis le fichier CSS
+*/
   return (
     <div className="calendar">
       <h1>Emploi du temps</h1>
-      <IframeResizer log src={URL} height="870px" width="1550px" />
+      <iframe className="IframeADE" src={URL} scrolling="no" />
     </div>
   );
 }
 
 //ancien (deprecated) : src="http://applisjava.insa-rennes.fr/MonAde6Image/html/images_ade/HugoLAMOUREUX5413.gif"
+
