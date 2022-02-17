adcc3ba5eea956e95c1b4d4f4cf8ef53c4802644
Hugo LICHTENBERGER <76788589+LICHTENBERGERHugo@users.noreply.github.com>
2022-02-17 23:25:39 +0100

    Revert "Fonction menu du self"

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,12 +4,11 @@ const Menu = () => {
   return (
     <div className="menu">
       <h1>Menu du self</h1>
-        {/* <object data="C:\Users\Hugo\Downloads\Menu-50.pdf" type="application/x-pdf" title="SamplePdf" alt="MenuSelf" height="450">
-        </object> */}
-        <a href="../Menu-50 (4).pdf">
-        <button onclick="window.location.href = '../Menu.pdf';">Menu du self</button>
-        </a>
-
+      <img
+        src=""
+        alt="MenuSelf"
+        height="450"
+      />
     </div>
   );
 };
