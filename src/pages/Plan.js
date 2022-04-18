9ae81cd1a034310a7dafea7a576d45db95fca713
francoisLEROUX <francois3443@gmail.com>
2022-04-18 13:53:42 +0200

    Modification du json et du js du plan sans impact sur la fonctionnalité, juste de la mise en forme

--- src/pages/Plan.js
+++ src/pages/Plan.js
@@ -2,19 +2,12 @@ import React from "react";
 import Navigation from "../components/Navigation";
 // import "../../styles/components/plan.scss";
 
-function info(num) {
-  let json = require("./batiments.json");
+function info(type, num) {
+  let json = require("./infos-plan.json");
   let infoBat = document.getElementById("info");
-  infoBat.innerText = "\nBâtiment " + num + " :" + json[num];
-}
-
-function infoBus(nom) {
-  let json = require("./bus.json");
-  let infoBus = document.getElementById("info");
-  infoBus.innerText =
-    "\nArrêt de bus " +
-    json[nom] +
-    "\nVous pouvez consulter les horaires de passage dans l'onglet 'Acceuil.'";
+  const elem =
+    type === "bus" ? "\nArrêt de bus " + num : "\nBâtiment " + num + " :\n";
+  infoBat.innerText = elem + json[type][num];
 }
 
 const About = () => {
@@ -35,163 +28,163 @@ const About = () => {
       <button
         className="bat1"
         onClick={() => {
-          info(1);
+          info("batiment", 1);
         }}
       ></button>
       <button
         className="bat2"
         onClick={() => {
-          info(2);
+          info("batiment", 2);
         }}
       ></button>
       <button
         className="bat3"
         onClick={() => {
-          info(3);
+          info("batiment", 3);
         }}
       ></button>
       <button
         className="bat4"
         onClick={() => {
-          info(4);
+          info("batiment", 4);
         }}
       ></button>
       <button
         className="bat5"
         onClick={() => {
-          info(5);
+          info("batiment", 5);
         }}
       ></button>
       <button
         className="bat6"
         onClick={() => {
-          info(6);
+          info("batiment", 6);
         }}
       ></button>
       <button
         className="bat7"
         onClick={() => {
-          info(7);
+          info("batiment", 7);
         }}
       ></button>
       <button
         className="bat8"
         onClick={() => {
-          info(8);
+          info("batiment", 8);
         }}
       ></button>
       <button
         className="bat9"
         onClick={() => {
-          info(9);
+          info("batiment", 9);
         }}
       ></button>
       <button
         className="bat10"
         onClick={() => {
-          info(10);
+          info("batiment", 10);
         }}
       ></button>
       <button
         className="bat11"
         onClick={() => {
-          info(11);
+          info("batiment", 11);
         }}
       ></button>
       <button
         className="bat12"
         onClick={() => {
-          info(12);
+          info("batiment", 12);
         }}
       ></button>
       <button
         className="bat13"
         onClick={() => {
-          info(13);
+          info("batiment", 13);
         }}
       ></button>
       <button
         className="bat14"
         onClick={() => {
-          info(14);
+          info("batiment", 14);
         }}
       ></button>
       <button
         className="bat15"
         onClick={() => {
-          info(15);
+          info("batiment", 15);
         }}
       ></button>
       <button
         className="bat16"
         onClick={() => {
-          info(16);
+          info("batiment", 16);
         }}
       ></button>
       <button
         className="bat17"
         onClick={() => {
-          info(17);
+          info("batiment", 17);
         }}
       ></button>
       <button
         className="bat18"
         onClick={() => {
-          info(18);
+          info("batiment", 18);
         }}
       ></button>
       <button
         className="bat19"
         onClick={() => {
-          info(19);
+          info("batiment", 19);
         }}
       ></button>
       <button
         className="bat20"
         onClick={() => {
-          info(20);
+          info("batiment", 20);
         }}
       ></button>
       <button
         className="bat21"
         onClick={() => {
-          info(21);
+          info("batiment", 21);
         }}
       ></button>
       <button
         className="bat22"
         onClick={() => {
-          info(22);
+          info("batiment", 22);
         }}
       ></button>
       <button
         className="bat23"
         onClick={() => {
-          info(23);
+          info("batiment", 23);
         }}
       ></button>
       <button
         className="bat24"
         onClick={() => {
-          info(24);
+          info("batiment", 24);
         }}
       ></button>
       <button
         className="busINSA"
         onClick={() => {
-          infoBus("INSA");
+          info("bus", "Beaulieu - INSA");
         }}
       ></button>
       <button
         className="busRU"
         onClick={() => {
-          infoBus("RU");
+          info("bus", "Beaulieu - RU");
         }}
       ></button>
       <button
         className="busIUT"
         onClick={() => {
-          infoBus("IUT");
+          info("bus", "IUT");
         }}
       ></button>
     </div>
